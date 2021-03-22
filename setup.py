8724224b80935fe900febb9d3ae306601171b8c2
Andreas Rabenstein <andreas.rabenstein@physik.uni-regensburg.de>
2021-03-22 23:39:39 +0100

    chore: Basic layout

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="flask_api",
+    author="Andreas Rabenstein",
+    email="andreas@rabenstein-page.de",
+    packages=find_packages(),
+    version="0.0.0",
+)
